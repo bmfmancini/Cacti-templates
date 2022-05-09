15605db23ed9819d8e8dd3e959836c4d181998f3
bmfmancini <sean@seanmancini.com>
2022-05-08 21:37:41 -0400

    update comments

--- apache-template/ss_apache_stats.php
+++ apache-template/ss_apache_stats.php
@@ -1,50 +1,8 @@
 <?php
 /*
-
-	ApacheStats 0.8.2 by RCK - http://forums.cacti.net/about25227.html
-
-	---------------
-	Changelog 0.8.2
-	---------------
-	- export was done with cacti 0.8.7e
-	- ss_apache_stats.php: add support for apache 1.x - BusyServers,IdleServers (thanks helzerr)
-	- ss_apache_stats.php: no more floor() on CPU query to have better values (thanks helzerr)
-	- fixed the CDEFs function for graph D and F (thanks kilos / spkane)
-	- fixed some multiple graph comments after import.
-
-	---------------
-	Changelog 0.8.1
-	---------------
-	- export was done with cacti 0.8.7b
-	- CDEF were updated to correctly works with one consolidation function (AVERAGE)
-	- fixed some graph template that were still using the (MAX) consolidation. 
-
-	---------------
-	Changelog 0.8.0
-	---------------
-	- data splitted into multiple RRD files to save 87% disk space (708K instead of 5400K)
-	- no more thread_O useless RRA
-	- no more redondant RRD data
-	- fix some useless "Graph Item Inputs"
-	- cacti 0.8.7 support
-	- this version of ss_apache_stats.php is compatible with ApacheStats 0.64
-
-	Usage:
-	------
-	From the command line:
-		php ss_apache_stats.php <hostname> <section> 
-	
-	As a script server object:
-		ss_apache_stats.php ss_apache_stats <hostname> <section>
-
-	Where <hostname> is the hostname of the webserver you are graphing against,
-	and <section> is one of the following data filter:
-	- workers
-	- kbytes
-	- hits
-	- cpuload
-	- threads
-	undefined -> old behaviour, all data are sent to cacti.
+        ORIGINAL WORK BY 
+        ApacheStats 0.8.2 by RCK - http://forums.cacti.net/about25227.html
+        Template uplifted by bmfmancini test on Cacti 1.2.x
 
 */
 
